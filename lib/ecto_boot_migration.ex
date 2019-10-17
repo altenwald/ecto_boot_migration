7b013cea7c12b5184d359b4016ca16ae86e787cf
Manuel Rubio <manuel@altenwald.com>
2019-10-17 19:31:07 +0200

    fix for new Ecto release

--- lib/ecto_boot_migration.ex
+++ lib/ecto_boot_migration.ex
@@ -50,7 +50,7 @@ defmodule EctoBootMigration do
     :crypto,
     :ssl,
     :postgrex,
-    :ecto
+    :ecto_sql
   ]
 
   @doc """
@@ -140,7 +140,7 @@ defmodule EctoBootMigration do
       |> Enum.reduce([], fn repo, acc ->
         log("Starting repo: #{inspect(repo)}")
 
-        case repo.start_link(pool_size: 2) do
+        case repo.start_link(pool_size: 10) do
           {:ok, pid} ->
             log("Started repo: pid = #{inspect(pid)}")
             [pid | acc]
